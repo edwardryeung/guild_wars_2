84491d0236b2d88961e35d2d84bb84fe1ea5e52e
Edward <eyeung3629@conestogac.on.ca>
2023-08-06 23:47:58 -0400

    azure stuff again

--- guild_wars_2/settings.py
+++ guild_wars_2/settings.py
@@ -86,18 +86,18 @@ WSGI_APPLICATION = 'guild_wars_2.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': 'postgres',
-    #    'USER': 'edwardryeung', #or the username you picked when creating the Postgres admin
-    #    'PASSWORD': 'slkcf9In@',
-    #    'HOST': 'gw2doorway-postgres-db.postgres.database.azure.com', #should end with postgres.database.azure.com
-    #    'PORT': '5432',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'edwardryeung', #or the username you picked when creating the Postgres admin
+        'PASSWORD': 'publicpassword1!',
+        'HOST': 'gw2doorway-postgres-db.postgres.database.azure.com', #should end with postgres.database.azure.com
+        'PORT': '5432',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
